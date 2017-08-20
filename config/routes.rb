4e1310a374637822f76c4d8b3c47d19633634c5c
Sean <gilmore.seven@gmail.com>
2017-08-20 17:26:10 +1000

    Init pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #defines a resource
+  resources :pages
+  # sets the initally loaded page
   root 'pages#index'
 end
